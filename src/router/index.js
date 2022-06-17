dd5c68d87a7d38d8071f9b6fa4e88d88d589714f
alexandrLosich <alexandr.losich@iptel.co>
2022-06-17 17:36:56 +0300

    Add private rout

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,18 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import SignIn from '../components/sign_in'
 import SignUp from '../components/sign_up'
-
-
+import PrivateRoute from './private_router'
 
 export default function Routs () {
 
   return (
-      <Routes>
-      {/*<Route exact path="/" element={<Home/>}/>*/}
-      {/*<Route exact path="/home" element={<Home/>}/>*/}
+    <Routes>
+      <Route exact path="/" element={<PrivateRoute
+        isAuthenticated={false}
+        children={{}}/>}/>
+      <Route exact path="/home" element={<PrivateRoute
+        isAuthenticated={false}
+        children={{}}/>}/>
       <Route exact path="/login" element={<SignIn/>}/>
       <Route path="/register" element={<SignUp/>}/>
     </Routes>
